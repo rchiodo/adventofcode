cf41f9da1d26b6eef57c23c68d8d2bb28c0e8ff2
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-12-05 14:17:43 -0800

    First day

---
+++ 2022/day1.py
@@ -0,0 +1,32 @@
+import os
+
+input_txt = os.path.join(os.path.dirname(__file__), "input.txt")
+lines: list[str] = []
+
+with open(input_txt, "r") as f:
+    lines = f.read().splitlines()
+    
+
+class Elf:
+    def __init__(self: "Elf", lines: list[int]):
+        self.calories = sum(lines)
+        
+    def __repr__(self: "Elf") -> str:
+        return f"Elf : {self.calories}"
+    
+elves: list[Elf] = []
+current = []
+
+for l in lines:
+    if len(l) > 1:
+        current.append(int(l))
+    elif len(current) > 0:
+        elves.append(Elf(current))
+        current = []
+
+inorder = sorted(elves, key=lambda e: e.calories, reverse=True)
+
+print("Highest calories:")
+print(inorder[0].calories)
+print("Combined calories of top 3")
+print(inorder[0].calories + inorder[1].calories + inorder[2].calories)
