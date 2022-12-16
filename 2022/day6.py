d9e0bac6bdb6b453a309868f72ad2cab5928953e
Rich Chiodo <rchiodo@users.noreply.github.com>
2022-12-15 17:00:30 -0800

    Day 6

---
+++ 2022/day6.py
@@ -0,0 +1,14 @@
+from read_input import read_input
+
+line = read_input("input6.txt")[0]
+prev3 = line[0:3]
+
+def find_unique(length: int): 
+    for i in range(length, len(line)-1):
+        unique = set(line[i-length:i])
+        if len(unique) == length:
+            return i
+
+print(f"First start of packet marker: {find_unique(4)}")
+print(f"First start of message marker: {find_unique(14)}")
+
